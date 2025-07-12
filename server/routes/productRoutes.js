52d447e20e26b1594c504497607f5a16f42756eb
ganesh14u <paladugusaiganesh@gmail.com>
2025-07-12 17:23:59 +0530

    Server Updated

--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Product = require('../models/product.model');
+const Product = require('../models/Product');
 
 // ✅ Create Product
 router.post('/', async (req, res) => {
