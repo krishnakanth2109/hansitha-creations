f7f81c19887567c6f4f58139d618e8292fbdb30a
ganesh14u <paladugusaiganesh@gmail.com>
2025-07-16 13:42:33 +0530

    Updated  New

--- server/models/User.model.js
+++ server/models/User.model.js
@@ -1,17 +1,43 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   name: String,
   address: String,
-  cart: [{ productId: mongoose.Schema.Types.ObjectId, quantity: Number }],
-  wishlist: [mongoose.Schema.Types.ObjectId],
-  orders: [{
-    products: [{ productId: mongoose.Schema.Types.ObjectId, quantity: Number }],
-    total: Number,
-    createdAt: { type: Date, default: Date.now }
-  }]
+  cart: [
+    {
+      productId: { type: mongoose.Schema.Types.ObjectId, ref: "Product" },
+      quantity: Number,
+    }
+  ],
+  wishlist: [{ type: mongoose.Schema.Types.ObjectId, ref: "Product" }],
+  orders: [
+    {
+      products: [
+        {
+          productId: { type: mongoose.Schema.Types.ObjectId, ref: "Product" },
+          quantity: Number,
+        }
+      ],
+      total: Number,
+      createdAt: { type: Date, default: Date.now },
+    }
+  ]
 }, { timestamps: true });
+
+// 🔐 Hash password before save
+userSchema.pre('save', async function (next) {
+  if (this.isModified('password')) {
+    this.password = await bcrypt.hash(this.password, 10);
+  }
+  next();
+});
+
+// 🔐 Compare password method
+userSchema.methods.comparePassword = function (password) {
+  return bcrypt.compare(password, this.password);
+};
 
 module.exports = mongoose.model('User', userSchema);
